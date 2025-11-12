722066462c5455729b2b161231e8550dc8b832d3
get-some-code <monideepmistry365@gmail.com>
2025-11-12 13:16:27 +0000

    understanding react

--- 01.Basics/src/App.jsx
+++ 01.Basics/src/App.jsx
@@ -1,6 +1,13 @@
+import Chai from "./Chai"
 function App() {
   return (
-    <h1> Learning React </h1>
+    <>
+      <Chai/>
+      <h1>Hello React!</h1>
+      <p>Test Paragraph! 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, officia!
+      </p>
+    </>
   )
 }
 
