d608dc84ba624c89918b3ce5ee00a0c602bc619f
get-some-code <monideepmistry365@gmail.com>
2025-11-12 16:57:51 +0000

    introduction

---
+++ 01.Introduction/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+function App() {
+  return (
+    <>
+      <Chai/>
+      <h1>Hello React!</h1>
+      <p>Test Paragraph! 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, officia!
+      </p>
+    </>
+  )
+}
+
+export default App
