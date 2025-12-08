fdff67142e632e9fcdf381a56e6bb612151a2b53
get-some-code <monideepmistry365@gmail.com>
2025-12-08 07:30:05 +0000

    updated hooks

---
+++ 03.0.Hooks/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+
+  let [counter,setCounter] = useState(5)
+
+  // let counter = 5;
+
+  const addValue = ()=>{
+    setCounter(counter+1);
+    // document.getElementById('first').textContent = `Counter value: ${counter}`;
+    
+  }
+
+  const subtractValue = ()=>{
+    if (counter > 0){
+      setCounter(counter-1);
+    }
+    else return;
+    // document.getElementById('first').textContent = `Counter value: ${counter}`;
+  }
+
+  return (
+    <>
+
+    <h1>Namaste Javascript!</h1>
+    <h2 id='first'>Counter value: {counter}</h2>
+    <button onClick={addValue}>Increment Value</button>
+    <br />
+    <button onClick={subtractValue}>Decrement Value</button>
+
+    </>
+    )
+}
+
+export default App
