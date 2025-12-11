b95f6e0e6361acab1dae213dc0a564cc56635598
get-some-code <monideepmistry365@gmail.com>
2025-12-11 07:31:03 +0000

    updated Button.jsx

--- 00.Just-A-Fluff/src/components/Button.jsx
+++ 00.Just-A-Fluff/src/components/Button.jsx
@@ -1,11 +1,10 @@
-export default function Button({buttonText = "default", click}){
+export default function Button({ buttonText = "default", click }) {
     return (
         <button
             onClick={click}
-            className="px-6 py-2 text-white rounded-lg bg-gradient-to-r from-green-500 to-emerald-600 hover:from-green-600 hover:to-emerald-700 active:scale-95 transition-all font-semibold shadow-lg">
+            className="px-6 py-2 text-white rounded-lg bg-linear-to-r from-green-500 to-emerald-600 hover:from-green-600 hover:to-emerald-700 active:scale-95 transition-all font-semibold shadow-lg">
             {buttonText}
         </button>
 
     );
-    
 };
