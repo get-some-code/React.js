ece8201cfc45e0720c0c38c103e49446cfb7c7fe
get-some-code <monideepmistry365@gmail.com>
2025-12-12 07:42:03 +0000

    added github repo link

--- 09.Project 5/src/App.jsx
+++ 09.Project 5/src/App.jsx
@@ -20,27 +20,42 @@ function Navigation() {
 
   return (
     <nav className={`sticky top-0 z-50 shadow-md bg-gray-800/70 backdrop-blur-md p-4`}>
-      <div className={`flex justify-center`}>
-        <div className={`flex flex-wrap gap-3 justify-center`}>
+      <div className="max-w-7xl mx-auto flex items-center justify-center relative">
+
+        <div className="flex flex-wrap gap-3 justify-center">
           {sections.map((section) => (
             <button
               className={`px-5 py-2.5 rounded-lg font-medium bg-blue-600 text-white 
-              hover:bg-blue-500 active:scale-95 transition-all duration-300 
-              shadow-md hover:shadow-xl flex items-center gap-2 tracking-wide`}
+          hover:bg-blue-500 active:scale-95 transition-all duration-300 
+          shadow-md hover:shadow-xl flex items-center gap-2 tracking-wide`}
               key={section.id}
               onClick={() => {
-                document.getElementById(section.id)?.scrollIntoView({ behavior: "smooth" })
+                document.getElementById(section.id)?.scrollIntoView({ behavior: "smooth" });
               }}
             >
-              <span className={`text-lg`}>
-                {section.icon}
-              </span>
+              <span className="text-lg">{section.icon}</span>
               {section.label}
             </button>
           ))}
         </div>
+
+        <a
+          href="https://github.com/get-some-code/React.js/tree/main/09.Project%205"
+          target="_blank"
+          className="absolute right-0 top-1/2 -translate-y-1/2 
+                 p-3 rounded-xl bg-gray-900 text-white 
+                 hover:bg-gray-700 transition-all duration-300 
+                 shadow-lg hover:shadow-xl"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M12 .5C5.65.5.55 5.65.55 12c0 5.1 3.3 9.4 7.9 10.9.6.1.8-.3.8-.6v-2.1c-3.2.7-3.9-1.4-3.9-1.4-.5-1.2-1.1-1.5-1.1-1.5-.9-.6.1-.6.1-.6 1 .1 1.6 1 1.6 1 .9 1.6 2.5 1.1 3.1.8.1-.6.4-1.1.7-1.3-2.6-.3-5.3-1.3-5.3-5.9 0-1.3.5-2.4 1.2-3.3-.1-.3-.5-1.5.1-3 0 0 1-.3 3.3 1.2a11.3 11.3 0 0 1 6 0C18 5 19 5.3 19 5.3c.6 1.5.2 2.7.1 3 .8.9 1.2 2 1.2 3.3 0 4.6-2.7 5.6-5.3 5.9.4.3.8 1 .8 2.1v3.1c0 .3.2.7.8.6A10.99 10.99 0 0 0 23.45 12C23.45 5.65 18.35.5 12 .5z" />
+          </svg>
+        </a>
+
       </div>
     </nav>
+
+
   )
 };
 
